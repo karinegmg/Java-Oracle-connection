9f3343f497f9f0a858fcf50d26aabeba55859c73
miguelarauj1o <miguelarauj1o@gmail.com>
2015-01-06 15:47:20 -0300

    testes em evolução...

---
+++ src/tests/ClientTest.java
@@ -0,0 +1,29 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClientTest {
+
+	@Test
+	public final void testClient() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetCPF() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetNome() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public final void testGetTelefones() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
